e7759b09b288a6f60c72060b9234c487298e9286
Aryan Tuteja <topicly.web@gmail.com>
2024-02-19 22:16:13 -0700

    latest working code without css

--- src/App.js
+++ src/App.js
@@ -1,30 +1,108 @@
 import logo from './logo.svg';
 import './App.css';
-import SignIn from './components/auth/SignIn';
+
 
 function App() {
+  const login = () => {
+    // Add your login logic here
+  };
+
+  const register = () => {
+    // Add your register logic here
+  };
+
+  const myMenuFunction = () => {
+    // Add your menu function logic here
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <SignIn />
-      </header>
-      <body>
-        
-      </body>
+    <div className="wrapper">
+      <nav className="nav">
+        <div className="nav-logo">
+          <p>Topicly</p>
+        </div>
+        <div className="nav-menu" id="navMenu">
+          <ul>
+            <li><a href="#" className="link active">Home</a></li>
+            <li><a href="#" className="link">Our Team</a></li>
+            <li><a href="#" className="link">Articles</a></li>
+            <li><a href="#" className="link">Contact Us</a></li>
+          </ul>
+        </div>
+        <div className="nav-button">
+          <button className="btn white-btn" id="loginBtn" onClick={login}>Log In</button>
+          <button className="btn" id="registerBtn" onClick={register}>Sign Up</button>
+        </div>
+        <div className="nav-menu-btn">
+          <i className="bx bx-menu" onClick={myMenuFunction}></i>
+        </div>
+      </nav>
+
+      <div className="form-box">
+        <div className="login-container" id="login">
+          <div className="top">
+            <span>Don't have an account? <a href="#" onClick={register}>Sign Up</a></span>
+            <header>Login</header>
+          </div>
+          <div className="input-box">
+            <input type="text" className="input-field" placeholder="Username or Email" />
+            <i className="bx bx-user"></i>
+          </div>
+          <div className="input-box">
+            <input type="password" className="input-field" placeholder="Password" />
+            <i className="bx bx-lock-alt"></i>
+          </div>
+          <div className="input-box">
+            <input type="submit" className="submit" value="Sign In" />
+          </div>
+          <div className="two-col">
+            <div className="one">
+              <input type="checkbox" id="login-check" />
+              <label htmlFor="login-check"> Remember Me</label>
+            </div>
+            <div className="two">
+              <label><a href="#">Forgot password?</a></label>
+            </div>
+          </div>
+        </div>
+
+        <div className="register-container" id="register">
+          <div className="top">
+            <span>Have an account? <a href="#" onClick={login}>Login</a></span>
+            <header>Sign Up</header>
+          </div>
+          <div className="two-forms">
+            <div className="input-box">
+              <input type="text" className="input-field" placeholder="Firstname" />
+              <i className="bx bx-user"></i>
+            </div>
+            <div className="input-box">
+              <input type="text" className="input-field" placeholder="Lastname" />
+              <i className="bx bx-user"></i>
+            </div>
+          </div>
+          <div className="input-box">
+            <input type="text" className="input-field" placeholder="Email" />
+            <i className="bx bx-envelope"></i>
+          </div>
+          <div className="input-box">
+            <input type="password" className="input-field" placeholder="Password" />
+            <i className="bx bx-lock-alt"></i>
+          </div>
+          <div className="input-box">
+            <input type="submit" className="submit" value="Register" />
+          </div>
+          <div className="two-col">
+            <div className="one">
+              <input type="checkbox" id="register-check" />
+              <label htmlFor="register-check"> Remember Me</label>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
 
 export default App;
