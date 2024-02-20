a3adbcd61b857dc42ac90e3bb18fd71668c1ef6e
Topicly <153854518+Topicly@users.noreply.github.com>
2024-02-20 04:41:58 +0000

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1><main_page/></h1>
+        <h1>main_page</h1>
       </header>
     </div>
   );
