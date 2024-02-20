fa997b5649ef947095fa3e86ac360dd6352ced1d
Topicly <153854518+Topicly@users.noreply.github.com>
2024-02-20 04:29:35 +0000

    changed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SignIn from 'src/components/auth/SignIn.jsx';
+import main_page from 'public/main_page.html';
 
 function App() {
-  const [htmlContent, setHtmlContent] = useState('');
-
-  useEffect(() => {
-    fetch('/public/main_page.html')
-      .then(response => response.text())
-      .then(data => setHtmlContent(data));
-  }, []);
-
-  return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <main_page/>
+      </header>
+    </div>
+  );
 }
 
 export default App;
+
