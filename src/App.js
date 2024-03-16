43cd40136de9504fd2e51f43aa3f43a32bb39a9b
Aryan Tuteja <topicly.web@gmail.com>
2024-03-16 11:06:22 -0600

    git

--- src/App.js
+++ src/App.js
@@ -1,108 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 
-
-function App() {
-  const login = () => {
-    // Add your login logic here
-  };
-
-  const register = () => {
-    // Add your register logic here
-  };
-
-  const myMenuFunction = () => {
-    // Add your menu function logic here
-  };
-  
+export default function App() {
   return (
-    <div className="wrapper">
-      <nav className="nav">
-        <div className="nav-logo">
-          <p>Topicly</p>
-        </div>
-        <div className="nav-menu" id="navMenu">
-          <ul>
-            <li><a href="#" className="link active">Home</a></li>
-            <li><a href="#" className="link">Our Team</a></li>
-            <li><a href="#" className="link">Articles</a></li>
-            <li><a href="#" className="link">Contact Us</a></li>
-          </ul>
-        </div>
-        <div className="nav-button">
-          <button className="btn white-btn" id="loginBtn" onClick={login}>Log In</button>
-          <button className="btn" id="registerBtn" onClick={register}>Sign Up</button>
-        </div>
-        <div className="nav-menu-btn">
-          <i className="bx bx-menu" onClick={myMenuFunction}></i>
-        </div>
-      </nav>
-
-      <div className="form-box">
-        <div className="login-container" id="login">
-          <div className="top">
-            <span>Don't have an account? <a href="#" onClick={register}>Sign Up</a></span>
-            <header>Login</header>
-          </div>
-          <div className="input-box">
-            <input type="text" className="input-field" placeholder="Username or Email" />
-            <i className="bx bx-user"></i>
-          </div>
-          <div className="input-box">
-            <input type="password" className="input-field" placeholder="Password" />
-            <i className="bx bx-lock-alt"></i>
-          </div>
-          <div className="input-box">
-            <input type="submit" className="submit" value="Sign In" />
-          </div>
-          <div className="two-col">
-            <div className="one">
-              <input type="checkbox" id="login-check" />
-              <label htmlFor="login-check"> Remember Me</label>
-            </div>
-            <div className="two">
-              <label><a href="#">Forgot password?</a></label>
-            </div>
-          </div>
-        </div>
-
-        <div className="register-container" id="register">
-          <div className="top">
-            <span>Have an account? <a href="#" onClick={login}>Login</a></span>
-            <header>Sign Up</header>
-          </div>
-          <div className="two-forms">
-            <div className="input-box">
-              <input type="text" className="input-field" placeholder="Firstname" />
-              <i className="bx bx-user"></i>
-            </div>
-            <div className="input-box">
-              <input type="text" className="input-field" placeholder="Lastname" />
-              <i className="bx bx-user"></i>
-            </div>
-          </div>
-          <div className="input-box">
-            <input type="text" className="input-field" placeholder="Email" />
-            <i className="bx bx-envelope"></i>
-          </div>
-          <div className="input-box">
-            <input type="password" className="input-field" placeholder="Password" />
-            <i className="bx bx-lock-alt"></i>
-          </div>
-          <div className="input-box">
-            <input type="submit" className="submit" value="Register" />
-          </div>
-          <div className="two-col">
-            <div className="one">
-              <input type="checkbox" id="register-check" />
-              <label htmlFor="register-check"> Remember Me</label>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
